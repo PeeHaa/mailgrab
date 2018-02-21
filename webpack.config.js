058120ab95a9a8d1ed7106dbdfbda0a26c78891a
Pieter Hordijk <info@pieterhordijk.com>
2018-02-21 17:56:41 +0100

    Start of frontend

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const webpack = require('webpack');
+const WebpackBuildNotifierPlugin = require('webpack-build-notifier');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: {
+        'bundle': './assets/js/main.js',
+        'bundle.min': './assets/js/main.js'
+    },
+    devtool: "source-map",
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'public/js')
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            DEBUG: JSON.stringify(true)
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            include: /\.min\.js$/,
+            minimize: true
+        }),
+        new WebpackBuildNotifierPlugin({
+            title: 'MailGrab',
+            successSound: false
+        }),
+        new ExtractTextPlugin({
+            filename: "/../css/[name].css"
+        })
+    ],
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['./../../node_modules/babel-preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.scss/,
+                use: ExtractTextPlugin.extract({
+                    use: ["css-loader", "sass-loader"]
+                })
+            }
+        ]
+    },
+    node: {
+        fs: 'empty'
+    }
+};
