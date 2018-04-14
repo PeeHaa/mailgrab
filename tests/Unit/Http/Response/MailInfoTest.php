83c43cde13c426646a0bfc96418e5009d3216890
Pieter Hordijk <info@pieterhordijk.com>
2018-04-14 21:48:23 +0200

    haaaaaalhp

--- tests/Unit/Http/Response/MailInfoTest.php
+++ tests/Unit/Http/Response/MailInfoTest.php
@@ -50,7 +50,10 @@ class MailInfoTest extends TestCase
         $messageMock
             ->expects($this->once())
             ->method('getHeaders')
-            ->willReturn(['subject' => new HeaderBuffer('subject', 'SUBJECT')])
+            ->willReturnCallback(function() {
+                var_dump('CALLED!?');
+                return ['subject' => new HeaderBuffer('subject', 'SUBJECT')];
+            })
         ;
 
         $result = json_decode((string) new MailInfo(13, $messageMock), true);
@@ -65,17 +68,24 @@ class MailInfoTest extends TestCase
         $this->assertRegExp('~^[\d]{4}-[\d]{2}-[\d]{2}T[\d]{2}:[\d]{2}~', $result['data']['timestamp']);
     }
 
-    public function __toString(): string
+    public function xtestToStringHasFrom()
     {
-        return json_encode([
-            'type' => 'mail-info',
-            'data' => [
-                'id'        => $this->id,
-                'subject'   => $this->subject,
-                'timestamp' => (new \DateTime())->format(\DateTime::RFC3339_EXTENDED),
-                'from'      => $this->message->getFrom(),
-                'to'        => $this->message->getRecipients(),
-            ],
-        ]);
+        $messageMock = $this->createMock(Message::class);
+
+        $messageMock
+            ->expects($this->once())
+            ->method('getHeaders')
+            ->willReturn([])
+        ;
+
+        $messageMock
+            ->expects($this->once())
+            ->method('getFrom')
+            ->willReturn('from@example.com')
+        ;
+
+        $result = json_decode((string) new MailInfo(13, $messageMock), true);
+
+        $this->assertSame('from@example.com', $result['data']['from']);
     }
 }
