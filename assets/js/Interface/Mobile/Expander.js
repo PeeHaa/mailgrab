5cbb95abdf47c238e8197b6c30007d2f10577f4b
Pieter Hordijk <info@pieterhordijk.com>
2018-04-20 13:50:08 +0200

    Expand and collapse messages

--- assets/js/Interface/Mobile/Expander.js
+++ assets/js/Interface/Mobile/Expander.js
@@ -1,6 +1,7 @@
 export default class Expander {
     constructor() {
         this.expander = document.querySelector('header .expander');
+        this.messages = document.querySelector('#messages');
 
         this.expander.addEventListener('click', this.clickHandler.bind(this));
     }
@@ -17,9 +18,11 @@ export default class Expander {
 
     expand() {
         this.expander.classList.add('expanded');
+        this.messages.classList.add('expanded');
     }
 
     collapse() {
         this.expander.classList.remove('expanded');
+        this.messages.classList.remove('expanded');
     }
 }
