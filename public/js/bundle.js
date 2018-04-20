41e6cff90e42acc89bbcf75deb50161a6d6b48a3
Pieter Hordijk <info@pieterhordijk.com>
2018-04-20 14:28:09 +0200

    Fixed toolbar placement on mobile

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -16912,6 +16912,10 @@ var _Expander = __webpack_require__(158);
 
 var _Expander2 = _interopRequireDefault(_Expander);
 
+var _Toolbar3 = __webpack_require__(159);
+
+var _Toolbar4 = _interopRequireDefault(_Toolbar3);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -16930,6 +16934,7 @@ var Interface = function () {
         this.notifier = new _Notifier2.default();
 
         new _Expander2.default();
+        new _Toolbar4.default();
 
         this.activeItem = null;
 
@@ -18861,6 +18866,48 @@ var Expander = function () {
 }();
 
 exports.default = Expander;
+
+/***/ }),
+/* 159 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Toolbar = function () {
+    function Toolbar() {
+        _classCallCheck(this, Toolbar);
+
+        this.expander = document.querySelector('header .expander');
+        this.title = document.querySelector('header h1');
+        this.hamburger = document.querySelector('header .hamburger');
+        this.toolbar = document.querySelector('header > ul');
+
+        this.hamburger.addEventListener('click', this.showToolbar.bind(this));
+    }
+
+    _createClass(Toolbar, [{
+        key: 'showToolbar',
+        value: function showToolbar() {
+            this.expander.classList.add('menu-expanded');
+            this.title.classList.add('menu-expanded');
+            this.hamburger.classList.add('menu-expanded');
+            this.toolbar.classList.add('menu-expanded');
+        }
+    }]);
+
+    return Toolbar;
+}();
+
+exports.default = Toolbar;
 
 /***/ })
 /******/ ]);
