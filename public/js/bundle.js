732e899cd5535e94b5ff674f0be1b5acc4463f8a
Pieter Hordijk <info@pieterhordijk.com>
2018-04-20 16:36:46 +0200

    Fixed overflow on text view

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -18043,7 +18043,7 @@ var Text = function () {
                                     var style = _this.element.createElement('style');
 
                                     style.type = 'text/css';
-                                    style.innerHTML = 'pre {color: #a3a3a3;background: #292929;padding: 15px;}';
+                                    style.innerHTML = 'pre {color: #a3a3a3;background: #292929;padding: 15px;white-space:pre-wrap;word-break: break-word;}';
 
                                     _this.element.querySelector('head').appendChild(style);
                         });
