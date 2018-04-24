3dfb9e1c85feb636108a1529d45dd898083b5577
Pieter Hordijk <info@pieterhordijk.com>
2018-04-24 17:06:56 +0200

    Added server socket test

--- tests/Unit/Smtp/Socket/ServerTest.php
+++ tests/Unit/Smtp/Socket/ServerTest.php
@@ -2,9 +2,14 @@
 
 namespace PeeHaa\MailGrabTest\Unit\Smtp\Socket;
 
+use Amp\Delayed;
+use Amp\Loop;
 use Amp\Promise;
+use Amp\Socket\ClientSocket;
 use PeeHaa\MailGrab\Smtp\Log\Output;
+use PeeHaa\MailGrab\Smtp\Socket\ServerSocket;
 use PHPUnit\Framework\TestCase;
+use function Amp\Socket\connect;
 use function PeeHaa\MailGrab\listen;
 
 class ServerTest extends TestCase
@@ -20,10 +25,24 @@ class ServerTest extends TestCase
 
     public function testAcceptReturnsResolvesToServerSocket()
     {
-        $logger = $this->createMock(Output::class);
+        $this->markTestSkipped('Need to find a way to test this.');
 
-        $server = listen($logger, 'tcp://127.0.0.1:9996');
+        Loop::run(function () {
+            $logger = $this->createMock(Output::class);
 
-        $this->assertInstanceOf(Promise::class, $server->accept());
+            $server = listen($logger, 'tcp://127.0.0.1:9996');
+
+            Loop::delay(2000, function () use ($server) {
+                /** @var ClientSocket $socket */
+                $socket = yield connect('tcp://127.0.0.1:9996');
+
+                yield new Delayed(5000);
+
+                $socket->close();
+                $server->close();
+            });
+
+            $this->assertInstanceOf(ServerSocket::class, yield $server->accept());
+        });
     }
 }
