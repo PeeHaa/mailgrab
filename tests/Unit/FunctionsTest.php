4ca5700f2abd1551cae56ab157b806d3350c79a9
Pieter Hordijk <info@pieterhordijk.com>
2018-04-25 21:26:20 +0200

    Added missing functions tests

---
+++ tests/Unit/FunctionsTest.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+namespace PeeHaa\MailGrabTest\Unit;
+
+use Amp\Promise;
+use PeeHaa\MailGrab\Smtp\Log\Output;
+use PHPUnit\Framework\TestCase;
+use function PeeHaa\MailGrab\listen;
+
+class FunctionsTest extends TestCase
+{
+    public function testListenDoesNotThrowOnMissingScheme()
+    {
+        $logger = $this->createMock(Output::class);
+
+        $server = listen($logger, '127.0.0.1:9995');
+
+        $this->assertInstanceOf(Promise::class, $server->accept());
+
+        $server->close();
+    }
+
+    public function testListenThrowsOnInvalidScheme()
+    {
+        $logger = $this->createMock(Output::class);
+
+        $this->expectException(\Throwable::class);
+
+        $server = listen($logger, 'ftp://127.0.0.1:9995');
+
+        $this->assertInstanceOf(Promise::class, $server->accept());
+
+        $server->close();
+    }
+
+    public function testListenThrowsWhenNotBeingAbleToConnect()
+    {
+        $logger = $this->createMock(Output::class);
+
+        $this->expectException(\Throwable::class);
+
+        $server = listen($logger, 'tcp://0.0.0.1:9000');
+
+        $this->assertInstanceOf(Promise::class, $server->accept());
+
+        $server->close();
+    }
+}
